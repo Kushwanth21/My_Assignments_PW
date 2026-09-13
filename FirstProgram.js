8b98776e6febbecf38b157c2270d0af83633f416
Vudhayagiri Kushwanth <113534778+Kushwanth21@users.noreply.github.com>
2026-09-13 07:56:32 +0530

    Add files via upload
    First program

---
+++ FirstProgram.js
@@ -0,0 +1,15 @@
+console.log("Hello World")
+console.log(123)
+console.log(false)
+
+var track="hello world"
+var num=123
+var answer=false
+var val
+var value = null
+
+console.log(typeof track)
+console.log(typeof num)
+console.log(typeof answer)
+console.log(typeof val)
+console.log(typeof value)
